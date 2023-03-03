2e8a411fc3c9c2d0059ca82cd579d6c8bb6e1a1d
Yondu715 <maximvorobev57@gmail.com>
2023-03-04 01:02:39 +0700

    delete package front

--- front/src/script/view/viewUtils/images.js
+++
@@ -1,8 +0,0 @@
-let path = "resources/img/";
-
-export const images = {
-	"no_image": path + "no_image.png",
-	"logo": path + "logo.png",
-	"logout": path + "logout.png",
-	"dragAndDrop": path + "dragAndDrop.png",
-}
